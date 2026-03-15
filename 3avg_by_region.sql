654e3c295083449699a377f1079a6986de141d55
KirillPe <pechenkovbsn@gmail.com>
2026-03-15 20:50:24 +0300

    Clear master for full PR

--- 3avg_by_region.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    area_id,
-    ROUND(AVG(compensation_from), 2) AS avg_compensation_from,
-    ROUND(AVG(compensation_to), 2) AS avg_compensation_to,
-    ROUND(AVG((compensation_from + compensation_to)/2), 2) AS avg_compensation_both
-FROM vacancies
-GROUP BY area_id;
